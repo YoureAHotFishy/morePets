0793a1f2f63d49f52645e6dad19ceeceeba872d1
YoureAHotFishy <crisban08@gmail.com>
2022-11-27 18:30:49 -0500

    Added geckolib

---
+++ src/main/java/net/youreahotfishy/morepets/Main.java
@@ -0,0 +1,16 @@
+package net.youreahotfishy.morepets;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+
+	public static final String MODID = "modid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/youreahotfishy/morepets/MorePetsClient.java
@@ -0,0 +1,11 @@
+package net.youreahotfishy.morepets;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MorePetsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
